6c66d50aa3255ab925008077203f21db0adfd74e
Samuel Kwak <kwak123@gmail.com>
2018-02-22 14:21:23 -0500

    fixed edge case in util method adding R to SIR, also minifying CSS now

--- client/lib/util.js
+++ client/lib/util.js
@@ -2,7 +2,13 @@ export const routeOrganizer = (service, routes) => service.reduce((acc, routeGro
   routes.forEach((route) => {
     if (routeGroup.name.includes(route.route_id)) {
       if (!acc[routeGroup.name]) { acc[routeGroup.name] = []; }
-      acc[routeGroup.name].push(route);
+      if (routeGroup.name === 'SIR') {
+        if (route.route_id !== 'R') { 
+          acc[routeGroup.name].push(route); 
+        }
+      } else {
+        acc[routeGroup.name].push(route);
+      }
     }
   });
   return acc;
--- webpack.config.js
+++ webpack.config.js
@@ -20,7 +20,14 @@ module.exports = {
         test: /\.css$/,
         loader: ExtractTextPlugin.extract({
           fallback: 'style-loader',
-          use: 'css-loader'
+          use: [
+            {
+              loader: 'css-loader',
+              options: {
+                minimize: true
+              }
+            }
+          ]
         })
       }
     ]
